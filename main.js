e172646f259cf1aa7f88dcaa2b3e929d7599923d
RohinParthavShah <83176943+RohinParthavShah@users.noreply.github.com>
2021-12-30 10:00:37 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+img = "";
+
+rightWristY = 0;
+rightWristX = 0;
+scoreRightWrist = 0;
+
+
+
+
+
+ function preload() {
+  ball_touch_paddel = loadSound("ball_touch_paddel.wav");
+  missed = loadSound("missed.wav");
+}
+
+function setup(){
+canvas =  createCanvas(700,600);
+canvas.parent('canvas');
+
+video = createCapture(VIDEO);
+video.size(700, 600);
+video.hide();
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+
+    rightWristY = results[0].pose.rightWrist.y;
+    rightWristX = results[0].pose.rightWrist.x;
+    scoreRightWrist =  results[0].pose.keypoints[10].score;
+    console.log(scoreRightWrist);
+  }
+}
+
+
+function draw(){
+
+  background(0); 
+  image(video, 0, 0, 700, 600);
+
+}
